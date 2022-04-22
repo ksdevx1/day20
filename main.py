4d7f49e21e8872fc0f6703d824c18b775ec788e3
Kestas <k.sukevicius@gmail.com>
2022-04-22 08:01:56 +0300

    adds functionality to extend snake after collision with food, and trigger game over if head collides with tail

--- main.py
+++ main.py
@@ -29,11 +29,22 @@ while game_is_on:
     #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     #Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    #Detect collision with tail.
+    #if head collides with any segment in the tail:
+        #trigger game_over
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass #skipping head segment
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
